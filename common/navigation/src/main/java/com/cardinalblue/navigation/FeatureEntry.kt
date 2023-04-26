a48b96b0ba49e985718e32e23cfb3aace3d270bc
Patrik Aradi <aradipatrik@gmail.com>
2023-04-26 17:52:33 +0800

    add navigation command provider

--- common/navigation/src/main/java/com/cardinalblue/navigation/FeatureEntry.kt
+++ common/navigation/src/main/java/com/cardinalblue/navigation/FeatureEntry.kt
@@ -1,26 +1,25 @@
 package com.cardinalblue.navigation
 
 import androidx.navigation.NamedNavArgument
-import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.composable
 
 interface Input
 
 object EmptyInput : Input
 
 interface FeatureEntry<I : Input> {
-    val featureRoute: String
-
-    fun destination(input: I): NavigationCommand
-
-    val arguments: List<NamedNavArgument>
-
     fun NavGraphBuilder.navigation(navController: NavHostController)
 }
 
-fun FeatureEntry<EmptyInput>.destination(): NavigationCommand = destination(EmptyInput)
+interface NavigationCommandProvider<I : Input> {
+    val featureRoute: String
+
+    val arguments: List<NamedNavArgument>
+    fun destination(input: I): NavigationCommand
+}
+
+fun NavigationCommandProvider<EmptyInput>.destination(): NavigationCommand = destination(EmptyInput)
 
 fun NavGraphBuilder.addFeatureEntry(
     navController: NavHostController,
