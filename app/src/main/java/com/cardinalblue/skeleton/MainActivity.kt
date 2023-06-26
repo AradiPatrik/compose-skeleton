61b2b98bc7ff5ecd132245d1f19474dc6d7d1cc6
Patrik Aradi <aradipatrik@gmail.com>
2023-06-26 10:25:43 +0800

    clean up MainActivity

--- app/src/main/java/com/cardinalblue/skeleton/MainActivity.kt
+++ app/src/main/java/com/cardinalblue/skeleton/MainActivity.kt
@@ -3,24 +3,11 @@ package com.cardinalblue.skeleton
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.SideEffect
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.navigation.NavGraph.Companion.findStartDestination
-import androidx.navigation.NavOptions
-import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.cardinalblue.data.api.DataProvider
-import com.cardinalblue.featuredmovies.api.FeaturedMoviesFeatureEntry
-import com.cardinalblue.moviesearch.api.MovieSearchFeatureEntry
 import com.cardinalblue.navigation.CompositionLocals
 import com.cardinalblue.navigation.FeatureEntriesProvider
 import com.cardinalblue.navigation.NavigationManager
@@ -28,7 +15,6 @@ import com.cardinalblue.navigation.NavigationManagerImpl
 import com.cardinalblue.navigation.NavigationProvider
 import com.cardinalblue.navigation.injectedViewModel
 import com.cardinalblue.platform.PlatformProvider
-import com.cardinalblue.profile.api.ProfileFeatureEntry
 import com.cardinalblue.skeleton.navigation.App
 import com.cardinalblue.theme.SkeletonTheme
 
